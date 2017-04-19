b5b5f1471c38decdcf2f098181d1be5d54594ab4
Andres <ajgoza@gmail.com>
2017-04-19 09:13:22 -0700

    minor.

--- full_sim/b_times.m
+++ full_sim/b_times.m
@@ -8,7 +8,8 @@ function x = b_times( z, parms, mats )
 
 %--Initialize
     m = parms.m; n = parms.n; mg = parms.mg;
-    nq = (m-1)*n + (n-1)*m; lev = 1; %body is in first grid level
+    nq = (m-1)*n + (n-1)*m; 
+    lev = 1; %body is in first grid level
 
     circ = zeros( (m-1)*(n-1), mg ); stfn = circ; vflx = zeros(nq, 1);
 %--
@@ -28,21 +29,44 @@ function x = b_times( z, parms, mats )
 
 %--Solve Poisson problem for stfn
 
-    %Solve on coarser grid level to get bcs for first grid level
+    %BCs for Poisson problem (will be overwritten if mg > 1)
+    stbc = zeros( ngam, 1 );
+
+    %If more than 1 grid then use 2nd grid for bcs
     if mg > 1
 
+        %Solve on coarser grid level to get bcs for first grid level
         stfn = RCinv( circ(:,2), parms, mats );
 
-        %First need bcs from 
-        stbc = zeros( ngam, 1 );
-
-        %from explicit treatment of circulation
+        %Get bcs from this streamfunction on the 2nd gridlevel
         stbc = get_stfn_BCs( stbc, stfn, 1, parms );
-        % (1 because we are getting bcs for first grid level)     
+        % (1 because we are getting bcs for first grid level) 
+        
+        %Get on fine grid level
+        stfn = RCinv( circ(:,1) + stbc, parms, mats );  
+        
+    else
+        
+        %Get on fine grid level
+        stfn = RCinv( circ(:,1), parms, mats );
+        
     end
+    
+    
+%--
+
+%--Get velocity on first grid from stream function
+
+    vflx = curl( stfn, stbc, parms );
+
 %--
     
     
+%--Interpolate onto the body and scale by h
+
+    x = mats.E * vflx;
+
+%--
 
 
 
